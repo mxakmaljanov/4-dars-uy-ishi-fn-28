35185330d72aa6f80d53847f1861566831e998e4
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-10-04 08:27:05 +0500

    4-dars

---
+++ index.js
@@ -0,0 +1,103 @@
+// 1-misol
+//let k =+prompt("k sonini kiriting");
+//let n =+prompt("n sonini kiriting");
+//for (let i = 0; i < n; i++) {
+//    if (n > 0) {
+//        console.log(k);
+//    } else {
+//        console.log("n musbat bo'lishi kerak");
+//    }
+//}
+//2-misol
+//let a = +prompt("a sonini kiriting");
+//let b = +prompt("b sonini kiriting");
+//if (a < b) {
+//    let counter = 0;
+//    for (let i = a; i <= b; i++) {
+//        console.log(i);
+//        counter++;
+//    }
+//    console.log("Chiqadigan sonlar soni" + counter);
+//} else {
+//    console.log("a soni b dan kichik bo'lishi kerak");
+//}
+//3-misol
+//let a = +prompt("a sonini kiriting");
+//let b = +prompt("b sonini kiriting");
+//
+//if (a < b) {
+//    let counter = 0;
+//    for (let i = b - 1; i > a; i--) {
+//        console.log(i);
+//        counter++;
+//    }
+//    console.log("Chiqadigan sonlar soni" + counter);
+//} else {
+//    console.log("a soni b dan kichik bo'lishi kerak");
+//}
+//4-misol
+//let bkn = +prompt("Bir kg konfet narxini kiriting");
+//if (bkn > 0) {
+//    for (let kg = 1; kg <= 10; kg++) {
+//        console.log(kg + "kg kilo narxi" + kg * bkn);
+//    }
+//} else {
+//    console.log("Narx musbat bo'lishi kerak");
+//}
+//5-misol
+//let bkn = +prompt("Bir kg konfet narxini kiriting");
+//if (bkn > 0) {
+//    for (let i = 1; i <= 10; i++) {
+//        let kg = i / 10;
+//        console.log(kg + " kg kilo narxi" + kg * bkn);
+//    }
+//} else {
+//    console.log("Narx musbat bo'lishi kerak");
+//}
+//6-misol
+//let bkn = +prompt("Bir kg konfet narxini kiriting");
+//if (bkn > 0) {
+//    for (let i = 12; i <= 20; i += 2) {
+//        let kg = i / 10;
+//        console.log(kg + " kg konfet narxi" + kg * bir_kilo_narxi);
+//    }
+//} else {
+//    console.log("Narx musbat bo'lishi kerak");
+//}
+//7-misol
+//let a = +prompt("a sonini kiriting");
+//let b = +prompt("b sonini kiriting");
+//if (a < b) {
+//    let sum = 0;
+//    for (let i = a; i <= b; i++) {
+//        sum += i;
+//    }
+//    console.log("Yig'indisi" + sum);
+//} else {
+//    console.log("a soni b dan kichik bo'lishi kerak");
+//}
+//8-misol
+//let a = +prompt("a sonini kiriting: ");
+//let b = +prompt("b sonini kiriting: ");
+//if (a < b) {
+//    let kopaytma = 1;
+//    for (let i = a; i <= b; i++) {
+//        kopaytma *= i;
+//    }
+//    console.log("Ko'paytmasi" + kopaytma);
+//} else {
+//    console.log("a soni b dan kichik bo'lishi kerak");
+//}
+//9-misol
+//let a = +prompt("a sonini kiriting");
+//let b = +prompt("b sonini kiriting");
+//
+//if (a < b) {
+//    let ky = 0;
+//    for (let i = a; i <= b; i++) {
+//        ky += i * i;
+//    }
+//    console.log("Kvadratlar yig'indisi: " + ky);
+//} else {
+//    console.log("a soni b dan kichik bo'lishi kerak");
+//}
